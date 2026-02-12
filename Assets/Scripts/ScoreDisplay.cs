2adb23c4dfd2b72eda1c23070227bef77e87d460
Vince van Gestel <v.vangestel@student.fontys.nl>
2026-02-12 15:32:57 +0100

    added highscore (actually including the changes xD)

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -3,21 +3,23 @@ using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
 {
-    public Image[] digits; // Assign 6 Image components in inspector, left-to-right or right-to-left
+    public Image[] digits; // 6 digits
     public Sprite[] numberSprites; // 0-9 sprites
+    public bool displayHighScore = false; // if true, show high score instead of current score
 
     void Update()
     {
-        UpdateScore(ScoreManager.Instance.score);
+        int value = displayHighScore ? ScoreManager.Instance.highScore : ScoreManager.Instance.score;
+        UpdateScore(value);
     }
 
     void UpdateScore(int score)
     {
-        string scoreStr = score.ToString().PadLeft(digits.Length, '0'); // ensures 6 digits
+        string scoreStr = score.ToString().PadLeft(digits.Length, '0');
 
         for (int i = 0; i < digits.Length; i++)
         {
-            int digit = scoreStr[i] - '0'; // convert char '0'-'9' to int
+            int digit = scoreStr[i] - '0';
             digits[i].sprite = numberSprites[digit];
         }
     }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,17 +5,38 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager Instance;
 
     public int score;
+    public int highScore; // track the high score
 
     void Awake()
     {
         if (Instance == null)
+        {
             Instance = this;
+            DontDestroyOnLoad(gameObject); // make highscore persist across scenes
+        }
         else
+        {
             Destroy(gameObject);
+        }
+
+        // Load high score from previous sessions
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     public void AddScore(int amount)
     {
         score += amount;
+
+        // Update high score if broken
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore); // save it
+        }
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
     }
 }
